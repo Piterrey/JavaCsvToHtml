fb986510e2af7a8bccf010de51999e9e9f13a602
Davide Canale <fuzzuca@gmail.com>
2013-10-12 12:59:13 +0200

    Update JavaCsvToHtml.java

--- JavaCsvToHtml.java
+++ JavaCsvToHtml.java
@@ -23,8 +23,8 @@ class MainFrame extends JFrame{
     }
 
     private void InitUI(){
-        final JTextField txtFileIn = new JTextField("/home/generic/input",10);
-        final JTextField txtFileOut = new JTextField("/home/generic/input",10);
+        final JTextField txtFileIn = new JTextField("/home/generic/prova.csv",10);
+        final JTextField txtFileOut = new JTextField("/home/generic/index.html",10);
         JButton btnEsegui = new JButton("Esegui");
 
         JPanel panel = new JPanel();
@@ -41,50 +41,61 @@ class MainFrame extends JFrame{
             public void actionPerformed(ActionEvent actionEvent) {
                 File fileIn = new File(txtFileIn.getText());
 
-                PrintWriter fileOut = null;
-                try{
-                fileOut = new PrintWriter(txtFileOut.getText());
-                }
-                catch (FileNotFoundException e) {   }
+                FileHTML html = new FileHTML(txtFileOut.getText());
+                html.createHTML(fileIn);
+                html.close();
 
-
-                HeaderHTML(fileOut);
-                BodyHTML(fileIn,fileOut);
-                FooterHTML(fileOut);
-
-                fileOut.close();
-            }
-
-            private void HeaderHTML(PrintWriter output){
-                //TODO: <!DOCTYPE html><html><header><title>Tabella da CSV</title></header><body><table border="1">
-                output.println("<!DOCTYPE html><html><header><title>Tabella da CSV</title><link rel=\"stylesheet\" type=\"text/css\" href=\"./base.css\"></header><body><table>");
-            }
-
-            private void FooterHTML(PrintWriter output){
-                //TODO: </table></body></html>
-                output.println("</table></body></html>");
-            }
-
-            private void BodyHTML(File input, PrintWriter output){
-                Scanner scanner = null;
-                try{
-                    scanner = new Scanner(input);
-                }catch (FileNotFoundException e){   };
-
-                while (scanner.hasNext()){
-                    String row = scanner.nextLine();
-                    //todo: <tr>
-                    output.println("<tr>");
-                    String[] fields = row.split(";");
-                    for(String field:fields){
-                        //todo: <td>field</td>
-                        output.format("<td>%s</td>",field);
-                    }
-                    //todo: </tr>
-                    output.println("</tr>");
-                }
-                scanner.close();
             }
         });
+    }
+}
+
+class FileHTML{
+    private PrintWriter fileOut;
+
+    public FileHTML(String path){
+        try{
+            this.fileOut = new PrintWriter(path);
+        }
+        catch (FileNotFoundException e) {   }
+    }
+
+    public void createHTML(File input){
+        HeaderHTML();
+        BodyHTML(input);
+        FooterHTML();
+    }
+
+    public void close(){
+        this.fileOut.close();
+    }
+
+    private void HeaderHTML(){
+        fileOut.println("<!DOCTYPE html><html><header><title>Tabella da CSV</title><link rel=\"stylesheet\" type=\"text/css\" href=\"./base.css\"></header><body><table>");
+        fileOut.flush();
+    }
+
+    private void FooterHTML(){
+        fileOut.println("</table></body></html>");
+        fileOut.flush();
+    }
+
+    private void BodyHTML(File input){
+        Scanner scanner = null;
+        try{
+            scanner = new Scanner(input);
+        }catch (FileNotFoundException e){   };
+
+        while (scanner.hasNext()){
+            String row = scanner.nextLine();
+            fileOut.println("<tr>");
+            String[] fields = row.split(";");
+            for(String field:fields){
+                fileOut.format("<td>%s</td>",field);
+            }
+            fileOut.println("</tr>");
+        }
+        scanner.close();
+        fileOut.flush();
     }
 }
